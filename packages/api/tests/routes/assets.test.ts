fc80323c54e923688b5a2e15253471f4f4fa1c5e
Tarun-k-Gupta <tarungupta_er@rediffmail.com>
2024-02-08 10:00:53 +0530

    initial commit

--- packages/api/tests/routes/assets.test.ts
+++ packages/api/tests/routes/assets.test.ts
@@ -1,19 +1,34 @@
 import request from "supertest";
-const express = require('express');
-
-import { server } from "../../src/server"
+import { server } from "../../src/server";
 
 
-server.post('assets/upload', function(req, res) {
-//   res.status(200).json({ nonce: 'y5dfghvou' });
-});
-
-request(server)
-  .post('assets/upload')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '12')
-  .expect(200)
-  .end(function(err) {
-    if (err) throw err;
+// Test case for uploading media with an empty string
+test("Upload media with an empty string", () => {
+  server.post("/assets/upload", (req, res) => {
+    if (!req.body.media) {
+      res.status(400).json({ error: "Missing media" });
+    } else {
+      res.status(200).json({ nonce: "y5dfghvou" });
+    }
   });
 
+  return request(server)
+    .post("/assets/upload")
+    .send({ media: "" })
+    .expect("Content-Type", /json/)
+    .expect(400)
+    .expect({ error: "Missing media" });
+});
+
+// Test case for uploading media with valid media data
+// test("Upload media with valid media data", () => {
+//   server.post("/assets/upload", (req, res) => {
+//     res.status(200).json({ nonce: "y5dfghvou" });
+//   });
+
+//   return request(server)
+//     .post("/assets/upload")
+//     .send({ media: "test" })
+//     .expect(200)
+//     .expect({ nonce: "y5dfghvou" });
+// });
--- packages/api/tests/routes/auth.test.ts
+++ packages/api/tests/routes/auth.test.ts
@@ -3,17 +3,16 @@ import request from "supertest";
 import { server } from "../../src/server"
 
 
-server.get('auth/nonce', function(req, res) {
-  res.status(200).json({ nonce: 'y5dfghvou' });
-});
-
-request(server)
-  .get('auth/nonce')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '12')
-  .expect(200)
-  .end(function(err) {
-    
-    if (err) throw err;
+test("Fetch nonce with GET request", () => {
+  server.get("/auth/nonce", (req, res) => {
+    res.status(200).json({ nonce: "y5dfghvou" });
   });
+
+  return request(server)
+    .get("/auth/nonce")
+    .expect("Content-Type", /json/)
+    .expect("Content-Length", "21")
+    .expect(200)
+    .expect({ nonce: "y5dfghvou" });
+});
 
